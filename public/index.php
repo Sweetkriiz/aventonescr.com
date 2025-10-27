ad15cf9326f6ef0c16410d3cea40fa843f38af0e
Krisley Castro <kriizcastro@hotmail.com>
2025-10-27 17:18:05 -0600

    Primer commit: estructura base del proyecto Aventones

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+$welcomeMessage = "Bienvenidos AventonesCR";
+$pageTitle = "aventoncesCR";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= htmlspecialchars($pageTitle) ?></title>
+</head>
+<body>
+   <h1><?= htmlspecialchars($welcomeMessage) ?></h1>
+</body>
+</html>
