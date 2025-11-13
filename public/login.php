8c2b9161b83837e495a1196b1438d7b9eb91741a
Krisley Castro <kriizcastro@hotmail.com>
2025-11-13 13:35:54 -0600

    Cambio de width y height en el logo

--- public/login.php
+++ public/login.php
@@ -100,7 +100,7 @@ unset($_SESSION["error"]);
     <meta charset="UTF-8">
     <title>Iniciar sesión - Aventones CR</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="../css/login.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 
